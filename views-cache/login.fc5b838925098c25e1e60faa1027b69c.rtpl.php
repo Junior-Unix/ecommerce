f7defb7505f993257e67f818b7b3a07b267b3944
Junior-Unix <sonjunior@live.com>
2021-10-03 17:15:15 -0300

    Configurando login e logout no arquivo index.php da raiz e nomeando deslogin e despassword em login.html do admin.

--- views-cache/login.fc5b838925098c25e1e60faa1027b69c.rtpl.php
+++ views-cache/login.fc5b838925098c25e1e60faa1027b69c.rtpl.php
@@ -33,13 +33,13 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="/res/admin/index2.html" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Login" name "deslogin">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" name="despassword">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
