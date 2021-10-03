99f925ad47b62ce77fe98d75f80d003dc5288eee
Junior-Unix <sonjunior@live.com>
2021-10-03 17:15:22 -0300

    Configurando login e logout no arquivo index.php da raiz e nomeando deslogin e despassword em login.html do admin.

--- index.php
+++ index.php
@@ -48,6 +48,24 @@ $app->get('/admin/login', function() {
 
 });
 
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
 $app->run();
 
  ?>
