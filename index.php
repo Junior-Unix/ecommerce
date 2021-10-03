9b71323d9c58db14f9f11f63d7acdd4c0beb3443
Junior-Unix <sonjunior@live.com>
2021-10-03 14:46:36 -0300

    Usado para testar *.html dentro da pasta views.

--- index.php
+++ index.php
@@ -7,10 +7,17 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+
+	//Usado para testar *.html dentro da pasta views.
+	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+	/*  
+//foi usado para testar o banco de dados. Ok!   
 	$sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	$results = $sql->select("SELECT * FROM tb_users"); */
 
 	echo json_encode($results);
 
