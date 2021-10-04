6f380dbc839223d2cb6af611244da16a69ee0006
Junior-Unix <sonjunior@live.com>
2021-10-04 00:20:18 -0300

    logout no template header.html.

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -62,7 +62,7 @@ class User extends Model {
 			||
 			!(int)$_SESSION[User::SESSION]["iduser"] > 0
 			||
-			(bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
 		) {
 			
 			header("Location: /admin/login");
@@ -77,7 +77,7 @@ class User extends Model {
 			||
 			!(int)$_SESSION[User::SESSION]["iduser"] > 0
 			||
-			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+			(bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
 		) {
 			
 			header("Location: /admin/login");
